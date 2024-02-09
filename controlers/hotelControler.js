ccef06a68926030aad11875406393cfe0ecf598a
Vytautas <v.stankeviciuss@gmail.com>
2024-02-09 10:54:59 +0200

    first

--- controlers/hotelControler.js
+++ controlers/hotelControler.js
@@ -1,56 +1,51 @@
 const fs = require('fs');
+const Hotel = require('../models/hotelModel');
 const hotels = JSON.parse(fs.readFileSync(`${__dirname}/../data/hotels.json`));
 //Callbacks
 
-exports.checkHotel = (req, res, next) => {
-    const hotel = hotels.find((hotel) => hotel.id == req.params.id);
-    if (req.params.id > hotels.length)
-    {
-        res.status(404).json({
-            status: 'failure',
-            message: 'invalid ID',
+exports.getAllHotels = async (req,res)=>{
+    try{
+        const hotels = await Hotel.find();
+        res
+        .status(200)
+        .json({
+            status:'sucess',
+            results:hotels.length,
+            data:{
+              hotels
+            }
         })
- 
-        return
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message:err
+        })
     }
- 
-    req.hotel = hotel
-    next()
-}
-
-exports.getAllHotels = (req,res)=>{
-    res
-    .status(200)
-    .json({
-        status:'sucess',
-        results:hotels.length,
-        data:{
-          hotels
-        }
-    })
 };
  
  
-exports.createHotel = (req, res)=>{
-    const newId = hotels[hotels.length-1].id+1;
-    console.log(newId);
-    const hotelData = Object.assign({id:newId}, req.body);
-    hotels.push(hotelData);
-    fs.writeFile(`${__dirname}/data/hotels.json`,JSON.stringify(hotels),err=>{
+exports.createHotel = async (req, res)=>{
+    try{
+        const newHotel = await Hotel.create(req.body)
         res
         .status(201)
         .json({
             status:'success',
             message: "New hotel created",
-            data: hotelData
+            data: newHotel
         })
-    })
+    }catch{
+        res.status(404).json({
+            status: 'failed',
+            message:err
+        })
+    }    
 };
  
-exports.getHotel = (req,res)=>{
-    console.log(req.params);
-    const hotel = hotels.find(el=>el.id === Number(req.params.id));
-    res
+exports.getHotel = async (req,res)=>{
+    try{
+        const hotel = await Hotel.findById(req.params.id)
+        res
     .status(200)
     .json({
         status:'success',
@@ -58,26 +53,55 @@ exports.getHotel = (req,res)=>{
             hotel
         }
     })
+    }catch{
+        res.status(404).json({
+            status: 'failed',
+            message:err
+        })
+    }
+    // const hotel = hotels.find(el=>el.id === Number(req.params.id));
+    
  
 };
  
-exports.updateHotel = (req,res)=>{
-    res
+exports.updateHotel = async(req,res)=>{
+    try{
+        const hotel = await Hotel.findByIdAndUpdate(req.params.id,req.body,{
+            runValidators: true
+        })
+        res
     .status(200)
     .json({
         status:'success',
         message: "Hotel Updated",
-        data: '<UPDATED>'
+        data: {
+            hotel
+        }
     })
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message:err
+        })
+    }
+    
 };
  
-exports.deleteHotel = (req,res)=>{
-    res
+exports.deleteHotel = async (req,res)=>{
+    try{
+        await Hotel.findByIdAndDelete(req.params.id);
+        res
     .status(200)
     .json({
         status:'success',
         message: "Hotel deleted",
         data: null
     })
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message:err
+        })
+    }
+    
 };
- 
--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router()
 const hotelController = require('../controlers/hotelControler');
  
-router.param('id',hotelController.checkHotel)
 router
 .route('/')
 .get(hotelController.getAllHotels)
